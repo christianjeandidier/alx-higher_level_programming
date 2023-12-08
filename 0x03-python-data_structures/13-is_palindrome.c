55d63a956fb9565f8e259acacc461804221960b4
christianjeandidier <ndo.jeandidier@gmail.com>
2023-12-07 18:51:12 -0800

    13. Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,34 +1,48 @@
 #include "lists.h"
 
 /**
- * palindrome - utility for is_palindrome
- * @top: pointer to a pointer to a singly linked list
- * @next: pointer to a singly linked list
- *
- * Return: integer, 1 if palindrome, else 0
- */
-int palindrome(listint_t **top, listint_t *next)
-{
-	int result = 0;
-
-	if (next == NULL)
-		return (1);
-
-	if (palindrome(top, next->next) && ((*top)->n == next->n))
-		result = 1;
-
-	*top = (*top)->next;
-
-	return (result);
-}
-
-/**
- * is_palindrome - check if a singly linked list is a palindrome
- * @head: linked list double pointer
- *
- * Return: integer, 1 if list is a palindrome else 0
+ * is_palindrome - tests if linked lists is palindrome
+ * @head: address of pointer to list
+ * Return: 1 is palindrome else 0
  */
 int is_palindrome(listint_t **head)
 {
-	return (palindrome(head, *head));
+	listint_t *slow = *head, *fast = *head, *node, *prev;
+	int failed = 0;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		slow = slow->next;
+	}
+	node = slow;
+	prev = NULL;
+	while (node)
+	{
+		fast = node->next;
+		node->next = prev;
+		prev = node;
+		node = fast;
+	}
+	fast = *head;
+	node = prev;
+	while (prev)
+	{
+		if (fast->n != prev->n)
+		{
+			failed = 1;
+			break;
+		}
+		fast = fast->next;
+		prev = prev->next;
+	}
+	prev = NULL;
+	while (node)
+	{
+		fast = node->next;
+		node->next = prev;
+		prev = node;
+		node = fast;
+	}
+	return (!failed);
 }
