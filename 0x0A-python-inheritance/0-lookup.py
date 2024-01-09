a9d738f3a8a5de87788ff5b08426ac69871b65b6
christianjeandidier <ndo.jeandidier@gmail.com>
2024-01-08 18:32:01 -0800

    0. Lookup

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""
-=============================
-Module with the method lookpu
-=============================
-"""
+''' function that returns the list
+'''
 
 
 def lookup(obj):
-    """Function for return the attributes for an object"""
-
+    ''' function: lookup()
+    Returns a list object
+    '''
     return dir(obj)
