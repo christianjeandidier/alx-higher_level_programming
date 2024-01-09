615291100da2076f3854ad711ee4839d58a2efbd
christianjeandidier <133997310+christianjeandidier@users.noreply.github.com>
2024-01-09 18:16:53 +0000

    Delete alx-higher_level_programming directory

--- alx-higher_level_programming/0x04-python-more_data_structures/5-number_keys.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/python3
-def number_keys(a_dictionary):
-    num_keys = len(a_dictionary)
-    return num_keys
--- alx-higher_level_programming/0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-def print_sorted_dictionary(a_dictionary):
-    sorted_dict = sorted(a_dictionary)
-    for i in sorted_dict:
-        print("{}: {}".format(i, a_dictionary[i]))
